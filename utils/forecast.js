5d95e36a5acb3a7a9056ae28331e9bb616987c49
Dev Vashishta <dev.vashishta@nagarro.com>
2020-06-28 16:23:50 +0530

    shortehand destructuing refactoring

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
 
     const url = `http://api.weatherstack.com/current?access_key=90e50391e7caa91fd424c03797cf8876&query=${latitude},${longitude}&units=f`
-request({url:url,json:true}, (error,response)=>{
+request({url,json:true}, (error,{body})=>{
 if(error){
  callback('unable to connect to weather service',undefined)
-}else if (response.body.error){
+}else if (body.error){
 callback('unable to find locatino', undefined)
 } else {
-    console.log('----------',response.body.current.temperature)
- callback(undefined,' It is currently ' + response.body.current.temperature + ' degress out. There is a ' + response.body.current.precip+ '% chance of rain.')
+    console.log('----------',body.current.temperature)
+ callback(undefined,' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip+ '% chance of rain.')
 }
 })
 }

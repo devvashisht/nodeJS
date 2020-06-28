5f9f810c10b580b0cb46f979cd307a43c021e8dd
Dev Vashishta <dev.vashishta@nagarro.com>
2020-06-28 14:38:54 +0530

    weather

---
+++ utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+
+    const url = `http://api.weatherstack.com/current?access_key=90e50391e7caa91fd424c03797cf8876&query=${latitude},${longitude}&units=f`
+request({url:url,json:true}, (error,response)=>{
+if(error){
+ callback('unable to connect to weather service',undefined)
+}else if (response.body.error){
+callback('unable to find locatino', undefined)
+} else {
+    console.log('----------',response.body.current.temperature)
+ callback(undefined,' It is currently ' + response.body.current.temperature + ' degress out. There is a ' + response.body.current.precipProbability + '% chance of rain.')
+}
+})
+}
+
+module.exports = forecast
